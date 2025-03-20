36c3fd628a85714ef4caf5bc8412f3263cd4d23f
Marius Niveri <mniveri@cc.systems>
2025-03-20 18:37:51 +0100

    nix: enable automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,12 @@
     experimental-features = nix-command flakes
   '';
 
+  gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
