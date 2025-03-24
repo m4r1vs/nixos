e642adccad31e70ec5d6dddb8cb77f45ac49ea24
Marius Niveri <mniveri@cc.systems>
2025-03-24 20:45:35 +0100

    hypridle: use hyprlock directly

--- home/services/hypridle.nix
+++ home/services/hypridle.nix
@@ -4,7 +4,7 @@
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
+        before_sleep_cmd = "hyprlock";
         after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
         ignore_dbus_inhibit = false;
         ignore_systemd_inhibit = false;
