7c0ed835e1ae49e2c0591483634063c74636bf63
Marius Niveri <mniveri@cc.systems>
2025-03-24 20:30:59 +0100

    system: add kmscon to replace geTTY, enable power saving on battery

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,33 @@
 
   services.xserver.videoDrivers = ["nvidia"];
 
+  services.kmscon = {
+    enable = true;
+    fonts = [
+      {
+        name = "JetBrainsMono NF";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+      }
+    ];
+    extraOptions = "--term xterm-256color";
+  };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 40;
+    };
+  };
+
   fonts.enableDefaultPackages = true;
 
   fonts.fontconfig = {
