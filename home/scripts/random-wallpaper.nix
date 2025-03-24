03164a84771647098a214e37c6cea8865ffb7ee3
Marius Niveri <mniveri@cc.systems>
2025-03-24 01:49:00 +0100

    wallpaper: use os notify

--- home/scripts/random-wallpaper.nix
+++ home/scripts/random-wallpaper.nix
@@ -3,6 +3,6 @@ pkgs.writeShellScript "random-wallpaper"
 # bash
 ''
   WALLPAPER="$(find ${builtins.path {path = ../wallpaper;}} -type f | shuf -n 1)"
-  ${pkgs.libnotify}/bin/notify-send "Showing "$(basename "$WALLPAPER")""
+  ${import ./nixos-notify.nix pkgs} -t 2200 "Now showing" "$(basename "$WALLPAPER")"
   ${pkgs.swww}/bin/swww img "$WALLPAPER" --transition-type any --transition-fps 60 --transition-step 20 --transition-duration 1
 ''
