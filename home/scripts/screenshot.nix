1f85e42bf2b450cf76a6a369a5c245c3e1ab5356
Marius Niveri <mniveri@cc.systems>
2025-03-24 11:20:46 +0100

    scripts/screenshot: add action to open in gimp/copy path

--- home/scripts/screenshot.nix
+++ home/scripts/screenshot.nix
@@ -2,19 +2,34 @@ pkgs:
 pkgs.writeShellScript "screenshot"
 # bash
 ''
-  OUTPUT="$HOME/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d-%H-%M-%S).png"
-  HYPR_OUTPUT="$(mktemp)"
-  ${pkgs.hyprshot}/bin/hyprshot -m region -o "/tmp" --freeze -f "tmp_screenshot.png" --silent 2> "$HYPR_OUTPUT"
+  HYPR_STDOUT="$(mktemp)"
+  ${pkgs.hyprshot}/bin/hyprshot -m region -o "/tmp" --freeze -f "tmp_screenshot.png" --silent 2> "$HYPR_STDOUT"
 
-  if cat $HYPR_OUTPUT | grep -q "cancelled"; then
+  if cat $HYPR_STDOUT | grep -q "cancelled"; then
     ${import ./nixos-notify.nix pkgs} -e "Screenshot Cancelled"
     exit 0
   fi
 
-  ${pkgs.swappy}/bin/swappy -f "/tmp/tmp_screenshot.png" -o "$OUTPUT"
-  if [ ! -f "$OUTPUT" ]; then
-    mv /tmp/tmp_screenshot.png "$OUTPUT"
+  FINAL_DESTINATION="/tmp"
+  OUTPUT="/tmp/tmp_screenshot.png"
+
+  if [[ "$1" == *"edit"* ]]; then
+    OUTPUT="$HOME/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d-%H-%M-%S).png"
+
+    ${pkgs.swappy}/bin/swappy -f "/tmp/tmp_screenshot.png" -o "$OUTPUT"
+    if [ ! -f "$OUTPUT" ]; then
+      mv /tmp/tmp_screenshot.png "$OUTPUT"
+    fi
+    FINAL_DESTINATION="~/Pictures/Screenshots"
   fi
-  ${pkgs.wl-clipboard}/bin/wl-copy < "$OUTPUT"
-  ${import ./nixos-notify.nix pkgs} -e -i "$OUTPUT" "Copied and saved!"
+
+  RESPONSE="$(${import ./nixos-notify.nix pkgs} -e -i "$OUTPUT" --action="gimp=Open in Gimp" --action="path=Copy Path" "Copied and saved to $FINAL_DESTINATION")"
+
+  if [[ "$RESPONSE" == *"gimp"* ]]; then
+      ${pkgs.gimp}/bin/gimp "$OUTPUT" &
+  fi
+
+  if [[ "$RESPONSE" == *"path"* ]]; then
+    echo "$OUTPUT" | ${pkgs.wl-clipboard}/bin/wl-copy
+  fi
 ''
