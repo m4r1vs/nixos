7b94c0c6c337fcafd8a2c60659f5f8c04fc5796c
Marius Niveri <mniveri@cc.systems>
2025-03-25 10:28:19 +0100

    fzf: add alt-k/j bindings to move up/down

--- home/fzf.nix
+++ home/fzf.nix
@@ -2,7 +2,7 @@
   enable = true;
   enableZshIntegration = false; # manually enable in zsh init
   defaultOptions = [
-    "--bind ctrl-e:preview-down,ctrl-y:preview-up"
+    "--bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down"
     "--preview-window=right,65%"
   ];
 }
--- home/zsh/init.nix
+++ home/zsh/init.nix
@@ -1,7 +1,7 @@
 pkgs:
 # zsh
 ''
-  export _ZO_FZF_OPTS="--tmux 80%,80% --bind ctrl-e:preview-down,ctrl-y:preview-up --preview \"FILE=\"{}\"; FILE=\"\$(echo \"\$FILE\" | sed \"s/\'//g\")\"; ${pkgs.lsd}/bin/lsd --git --icon=always --color=always --tree --depth=2 /\"\''${FILE#*/}\"\""
+  export _ZO_FZF_OPTS="--tmux 80%,80% --bind ctrl-e:preview-down,ctrl-y:preview-up,alt-k:up,alt-j:down --preview \"FILE=\"{}\"; FILE=\"\$(echo \"\$FILE\" | sed \"s/\'//g\")\"; ${pkgs.lsd}/bin/lsd --git --icon=always --color=always --tree --depth=2 /\"\''${FILE#*/}\"\""
   zvm_after_init_commands+=(eval "$(${pkgs.fzf}/bin/fzf --zsh)")
   zoxide_jump() {
     if zi; then
