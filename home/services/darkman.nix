9974936cad79f96bd3a0a7b5c3551c41afc4a650
Marius Niveri <mniveri@cc.systems>
2025-03-26 23:26:42 +0100

    darkman: do not notify when changing theme

--- home/services/darkman.nix
+++ home/services/darkman.nix
@@ -10,13 +10,11 @@
       mode = "${pkgs.dconf}/bin/dconf write\ /org/gnome/desktop/interface/color-scheme \"'prefer-dark'\"";
       theme = "${pkgs.dconf}/bin/dconf write\ /org/gnome/desktop/interface/gtk-theme \"'Adwaita-dark'\"";
       customs = "${import ../scripts/custom-theme.nix pkgs} dark";
-      notify = "${import ../scripts/nixos-notify.nix pkgs} -e -r 91191 -t 900 \"Dark Mode Activated\"";
     };
     lightModeScripts = {
       mode = "${pkgs.dconf}/bin/dconf write\ /org/gnome/desktop/interface/color-scheme \"'prefer-light'\"";
       theme = "${pkgs.dconf}/bin/dconf write\ /org/gnome/desktop/interface/gtk-theme \"'Adwaita'\"";
       customs = "${import ../scripts/custom-theme.nix pkgs} light";
-      notify = "${import ../scripts/nixos-notify.nix pkgs} -e -r 91191 -t 900 \"Welcome to the bright side :)\"";
     };
   };
 }
