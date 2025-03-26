0db1fbbc181fdea52f8e379a28cff81404106671
Marius Niveri <mniveri@cc.systems>
2025-03-26 10:15:58 +0100

    nvim/codecompanion: switch to Gemini 2.5

--- home/neovim/nvim/lua/plugins/codecompanion.lua
+++ home/neovim/nvim/lua/plugins/codecompanion.lua
@@ -55,12 +55,11 @@ return {
           },
           schema = {
             model = {
-              default = "gemini-2.0-flash",
+              default = "gemini-2.5-pro-exp-03-25",
             },
           },
         })
       end,
     },
   }
-
 }
