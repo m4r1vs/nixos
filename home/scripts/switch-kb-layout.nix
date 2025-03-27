2507766b92d2f21cd48ff2dc951439442200b772
Marius Niveri <mniveri@cc.systems>
2025-03-27 13:00:20 +0100

    scripts/switch-kb-layout: make notification replace earlier one

--- home/scripts/switch-kb-layout.nix
+++ home/scripts/switch-kb-layout.nix
@@ -4,5 +4,5 @@ pkgs.writeShellScript "switch-kb-layout"
 ''
   ${pkgs.hyprland}/bin/hyprctl switchxkblayout all next
   layMain=$(${pkgs.hyprland}/bin/hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
-  notify-send -e -r 91190 -t 800 -i "${pkgs.papirus-icon-theme}/share/icons/Papirus/128x128/devices/input-keyboard.svg" "''${layMain}"
+  notify-send -e -h string:synchronous:switch-kb-layout -t 1000 -i "${pkgs.papirus-icon-theme}/share/icons/Papirus/128x128/devices/input-keyboard.svg" "Switched Keyboard Layout:" "$layMain"
 ''
