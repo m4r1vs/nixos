f16060ae9be8fe84d5fd1d808785cfca1eafff0f
Marius Niveri <mniveri@cc.systems>
2025-03-28 13:18:36 +0100

    scripts/random-wallpaper: replace old notification instead of creating new one

--- home/scripts/random-wallpaper.nix
+++ home/scripts/random-wallpaper.nix
@@ -3,6 +3,6 @@ pkgs.writeShellScript "random-wallpaper"
 # bash
 ''
   WALLPAPER="$(find ${builtins.path {path = ../wallpaper;}} -type f | shuf -n 1)"
-  ${import ./nixos-notify.nix pkgs} -i "$WALLPAPER" -t 2200 "Now showing" "$(basename "$WALLPAPER")"
+  ${import ./nixos-notify.nix pkgs} -e -i "$WALLPAPER" -h string:synchronous:random-wallpaper -t 1800 "Now showing" "$(basename "$WALLPAPER")"
   ${pkgs.swww}/bin/swww img "$WALLPAPER" --transition-type any --transition-fps 60 --transition-step 20 --transition-duration 1
 ''
