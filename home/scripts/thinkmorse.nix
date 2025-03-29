fe0f4245496228f39d3f0aabf95f102aef8ab489
Marius Niveri <mniveri@cc.systems>
2025-03-29 14:42:56 +0100

    scripts/thinkmorse: disable logging

--- home/scripts/thinkmorse.nix
+++ home/scripts/thinkmorse.nix
@@ -28,85 +28,82 @@
     let
       ditDelay = config.services.thinkmorse.speed;
       thinkmorse = pkgs.writeShellScriptBin "thinkmorse" ''
-        modprobe -r ec_sys
-        modprobe ec_sys write_support=1
+        while [[ 1 ]]; do
+          led(){
+            ${lib.concatStringsSep "\n" (builtins.map (device: ''echo $1 | ${pkgs.coreutils}/bin/tee /sys/class/leds/${device}/brightness > /dev/null'') config.services.thinkmorse.devices)}
+          }
 
-        led(){
-          ${lib.concatStringsSep "\n" (builtins.map (device: ''echo $1 | ${pkgs.coreutils}/bin/tee /sys/class/leds/${device}/brightness'') config.services.thinkmorse.devices)}
-        }
+          dit(){
+                  led 1
+                  sleep ${ditDelay}
+                  led 0
+                  sleep ${ditDelay}
+          }
 
-        dit(){
-                led 1
-                sleep ${ditDelay}
-                led 0
-                sleep ${ditDelay}
-        }
+          dah(){
+                  led 1
+                  sleep ${ditDelay}
+                  sleep ${ditDelay}
+                  sleep ${ditDelay}
+                  led 0
+                  sleep ${ditDelay}
+          }
 
-        dah(){
-                led 1
-                sleep ${ditDelay}
-                sleep ${ditDelay}
-                sleep ${ditDelay}
-                led 0
-                sleep ${ditDelay}
-        }
+          morse(){
+                  case $1 in
+                          "0") dah; dah; dah; dah; dah;;
+                          "1") dit; dah; dah; dah; dah;;
+                          "2") dit; dit; dah; dah; dah;;
+                          "3") dit; dit; dit; dah; dah;;
+                          "4") dit; dit; dit; dit; dah;;
+                          "5") dit; dit; dit; dit; dit;;
+                          "6") dah; dit; dit; dit; dit;;
+                          "7") dah; dah; dit; dit; dit;;
+                          "8") dah; dah; dah; dit; dit;;
+                          "9") dah; dah; dah; dah; dit;;
+                          "a") dit; dah;;
+                          "b") dah; dit; dit; dit;;
+                          "c") dah; dit; dah; dit;;
+                          "d") dah; dit; dit;;
+                          "e") dit;;
+                          "f") dit; dit; dah; dit;;
+                          "g") dah; dah; dit;;
+                          "h") dit; dit; dit; dit;;
+                          "i") dit; dit;;
+                          "j") dit; dah; dah; dah;;
+                          "k") dah; dit; dah;;
+                          "l") dit; dah; dit; dit;;
+                          "m") dah; dah;;
+                          "n") dah; dit;;
+                          "o") dah; dah; dah;;
+                          "p") dit; dah; dah; dit;;
+                          "q") dah; dah; dit; dah;;
+                          "r") dit; dah; dit;;
+                          "s") dit; dit; dit;;
+                          "t") dah;;
+                          "u") dit; dit; dah;;
+                          "v") dit; dit; dit; dah;;
+                          "w") dit; dah; dah;;
+                          "x") dah; dit; dit; dah;;
+                          "y") dah; dit; dah; dah;;
+                          "z") dah; dah; dit; dit;;
+                          " ") sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay} ;;
+                  esac
+                  sleep 0.2;
+          }
 
-        morse(){
-                case $1 in
-                        "0") dah; dah; dah; dah; dah;;
-                        "1") dit; dah; dah; dah; dah;;
-                        "2") dit; dit; dah; dah; dah;;
-                        "3") dit; dit; dit; dah; dah;;
-                        "4") dit; dit; dit; dit; dah;;
-                        "5") dit; dit; dit; dit; dit;;
-                        "6") dah; dit; dit; dit; dit;;
-                        "7") dah; dah; dit; dit; dit;;
-                        "8") dah; dah; dah; dit; dit;;
-                        "9") dah; dah; dah; dah; dit;;
-                        "a") dit; dah;;
-                        "b") dah; dit; dit; dit;;
-                        "c") dah; dit; dah; dit;;
-                        "d") dah; dit; dit;;
-                        "e") dit;;
-                        "f") dit; dit; dah; dit;;
-                        "g") dah; dah; dit;;
-                        "h") dit; dit; dit; dit;;
-                        "i") dit; dit;;
-                        "j") dit; dah; dah; dah;;
-                        "k") dah; dit; dah;;
-                        "l") dit; dah; dit; dit;;
-                        "m") dah; dah;;
-                        "n") dah; dit;;
-                        "o") dah; dah; dah;;
-                        "p") dit; dah; dah; dit;;
-                        "q") dah; dah; dit; dah;;
-                        "r") dit; dah; dit;;
-                        "s") dit; dit; dit;;
-                        "t") dah;;
-                        "u") dit; dit; dah;;
-                        "v") dit; dit; dit; dah;;
-                        "w") dit; dah; dah;;
-                        "x") dah; dit; dit; dah;;
-                        "y") dah; dit; dah; dah;;
-                        "z") dah; dah; dit; dit;;
-                        " ") sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay}; sleep ${ditDelay} ;;
-                        #*) echo "done";;
-                esac
-                sleep 0.2;
-        }
-
-        parse(){
-                tmp=$1
-                for i in $(seq 0 ''${#tmp})
-                do
-                        echo "current letter: ''${tmp:$i:1}"
-                        morse ''${tmp:$i:1}
-                done
-        }
-        led 0
-        parse "${config.services.thinkmorse.message}"
-        led 0
-        sleep 1
+          parse(){
+                  tmp=$1
+                  for i in $(seq 0 ''${#tmp})
+                  do
+                          morse ''${tmp:$i:1}
+                  done
+          }
+          led 0
+          parse "${config.services.thinkmorse.message}"
+          led 0
+          sleep 1
+        done
       '';
     in {
       systemd.services.thinkmorse = {
