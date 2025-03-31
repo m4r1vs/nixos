3bad4d23c95f8d08e33603f95df072425f9b6325
Marius Niveri <mniveri@cc.systems>
2025-03-31 17:54:37 +0200

    nix: add community-cache

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,15 @@
       dates = "daily";
       options = "--delete-older-than 10d";
     };
+    settings = {
+      auto-optimise-store = true;
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   boot = {
