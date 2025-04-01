ebff1440b9ede6c5ef23e974162c71e2a71060d6
Marius Niveri <mniveri@cc.systems>
2025-04-01 03:54:48 +0200

    fonts: build ios 18 emoji

--- configuration.nix
+++ configuration.nix
@@ -301,17 +301,28 @@
       eb-garamond
       (stdenv.mkDerivation {
         name = "Apple Color Emoji Font";
-        src = fetchurl {
-          url = "https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf";
-          hash = "sha256-SG3JQLybhY/fMX+XqmB/BKhQSBB0N1VRqa+H6laVUPE=";
+        enableParallelBuilding = true;
+        src = fetchFromGitHub {
+          owner = "mistu01";
+          repo = "apple-emoji-linux";
+          rev = "ios-17.4";
+          sha256 = "sha256-k4RFvhvl8tdaFA3Mlp3+ql88lFyEShaPen4PApDlbDc=";
         };
-        dontUnpack = true;
+        buildInputs = [
+          which
+          python3
+          python3Packages.fonttools
+          python3Packages.nototools
+          optipng
+          zopfli
+          pngquant
+          gnumake
+          imagemagick
+        ];
         installPhase = ''
           runHook preInstall
-
           mkdir -p $out/share/fonts/truetype
-          cp $src $out/share/fonts/truetype/AppleColorEmoji.ttf
-
+          cp ./AppleColorEmoji.ttf $out/share/fonts/truetype
           runHook postInstall
         '';
       })
