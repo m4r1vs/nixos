3f59839dbd6546fbcfc4bdca95a1a7c32dbde73a
Marius Niveri <mniveri@cc.systems>
2025-04-02 10:54:23 +0200

    scripts/switch-kb-layout: introduce delay so correct layout is always printed

--- modules/home-manager/scripts/switch-kb-layout.nix
+++ modules/home-manager/scripts/switch-kb-layout.nix
@@ -6,8 +6,8 @@
   switch-kb-layout =
     pkgs.writeShellScript "switch-kb-layout"
     ''
-      ${pkgs.hyprland}/bin/hyprctl switchxkblayout all next
+      ${pkgs.hyprland}/bin/hyprctl switchxkblayout all next && sleep 0.05
       layMain=$(${pkgs.hyprland}/bin/hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
-      ${scripts.nixos-notify} -e -h string:synchronous:switch-kb-layout -t 1000 -i "${pkgs.papirus-icon-theme}/share/icons/Papirus/128x128/devices/input-keyboard.svg" "Switched Keyboard Layout:" "$layMain"
+      ${scripts.nixos-notify} -e -h string:synchronous:switch-kb-layout -t 1000 -i "${pkgs.papirus-icon-theme}/share/icons/Papirus/128x128/devices/input-keyboard.svg" "Switched Keyboard Layout to:" "$layMain"
     '';
 }
