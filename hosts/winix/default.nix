1017e400b20752a593fc121ae3355f3de77fc3b5
Marius Niveri <marius.niveri@gmail.com>
2025-04-14 15:46:36 +0200

    hosts/winix: add 1password-cli wrapper

--- hosts/winix/default.nix
+++ hosts/winix/default.nix
@@ -26,6 +26,13 @@
     (writeShellScriptBin "ssh-agent" "/mnt/c/WINDOWS/System32/OpenSSH/ssh-agent.exe $@")
     (writeShellScriptBin "ssh-keygen" "/mnt/c/WINDOWS/System32/OpenSSH/ssh-keygen.exe $@")
     (writeShellScriptBin "ssh-keyscan" "/mnt/c/WINDOWS/System32/OpenSSH/ssh-keyscan.exe $@")
+    (writeShellScriptBin "op" ''
+      mapfile -d op_env_vars < <(env -0 | grep -z ^OP_ | cut -z -d= -f1)
+      export WSLENV="''${WSLENV:-}:$(IFS=:; echo "''${op_env_vars
+
+      }")"
+      exec /mnt/c/Users/mariu/AppData/Local/Microsoft/WinGet/Links/op.exe "$@"
+    '')
   ];
 
   system = {
