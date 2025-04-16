214114130e1c8b1fedae7ea0ef644fb56978738d
Marius Niveri <marius.niveri@gmail.com>
2025-04-16 21:49:22 +0200

    hosts/kubenix: debug k8s

--- hosts/kubenix/falkenberg/default.nix
+++ hosts/kubenix/falkenberg/default.nix
@@ -1,35 +1,20 @@
-{systemArgs, ...}: let
-  domain = "kubenix-falkenberg.niveri.dev";
-  ipv4 = "91.99.10.215";
-  ipv4gateway = "172.31.1.1";
-  ipv6 = "2a01:4f8:c013:e704::1";
-  ipv6gateway = "fe80::1";
+{
+  systemArgs,
+  config,
+  ...
+}: let
+  inherit (systemArgs) ipv4;
+  k8sPort = 6443;
 in {
   imports = [
     ./disks.nix
     ./hardware-configuration.nix
   ];
 
-  services = {
-    comin = {
-      enable = false;
-      allowForcePushMain = true;
-      remotes = [
-        {
-          name = "origin";
-          url = "https://github.com/m4r1vs/NixConfig.git";
-          branches.main.name = "main";
-        }
-      ];
-    };
-  };
-
   configured.server = {
     enable = true;
     networking = {
       enable = true;
-      inherit ipv4 ipv6 ipv4gateway ipv6gateway;
-      interface = "enp1s0";
       nameservers = [
         "8.8.8.8"
       ];
@@ -38,11 +23,45 @@ in {
 
   networking = {
     firewall = {
-      allowedTCPPorts = [22];
+      allowedTCPPorts = [53 80 443 6443];
+      allowedUDPPorts = [53];
     };
   };
 
-  system = {
-    nixos.label = systemArgs.hostname + ".niveri.de";
+  services.kubernetes = {
+    roles = ["master" "node"];
+    apiserverAddress = "https://${config.networking.fqdn}:${toString k8sPort}";
+    masterAddress = config.networking.fqdn;
+    easyCerts = true;
+    addons.dns = {
+      enable = true;
+      clusterDomain = "kubenix.local";
+      coredns = {
+        imageName = "coredns/coredns";
+        imageDigest = "sha256:4779e7517f375a597f100524db6f7f8b5b8499a6ccd14aacfa65432d4cfd5789";
+        finalImageTag = "arm64-1.12.1";
+        sha256 = "sha256-z3TUo76w7CyYyf5Z6lVHw/F9nZQ501CZ9NNVFFWcX58=";
+      };
+    };
+    flannel = {
+      enable = true;
+      openFirewallPorts = true;
+    };
+    controllerManager = {
+      enable = true;
+    };
+    kubelet = {
+      enable = true;
+      hostname = config.networking.fqdn;
+    };
+    scheduler = {
+      enable = true;
+    };
+    apiserver = {
+      enable = true;
+      securePort = k8sPort;
+      advertiseAddress = ipv4;
+      allowPrivileged = true;
+    };
   };
 }
